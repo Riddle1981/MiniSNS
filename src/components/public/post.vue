e2d53d667456b6424e9c7aa7f745e3a006027238
liusimin9527 <1246920026@qq.com>
2017-08-24 11:14:37 +0800

    modify style

--- src/components/public/post.vue
+++ src/components/public/post.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="post">
     <div id="post-cover"></div>
@@ -15,7 +14,7 @@
 
         <textarea name="" id="" placeholder="添加话题"></textarea>
         <label>问题描述（可选）：</label>
-        <textarea name="" id="" placeholder="问题背景、条件等详细信息"></textarea>
+        <textarea name="" id="" placeholder="问题背景、条件等详细信息" rows="5"></textarea>
         <input type="checkbox" name="" id="">匿名提问
         <input class="btn" type="submit" value="提交问题">
       </div>
@@ -34,17 +33,14 @@
           // 隐藏发帖部分
           oContainer.style.display = 'none'
           oCover.style.display = 'none'
+          history.back()
         }
       }
     }
 </script>
 
 <style>
-.post {
-
-}
 #post-cover {
-  display: none;
   position: fixed;
   top: 0;
   left: 0;
@@ -55,7 +51,6 @@
   opacity: 0.5;
 }
 #post-container {
-  display: none;
   position: absolute;
   top: 50%;
   left: 50%;
@@ -70,27 +65,75 @@
   border: 0;
   outline: 0;
 }
+.view-header {
+    padding: 12px;
+    overflow: hidden;
+    border-bottom: 1px solid #e5e5e5;
+    min-height: 25px;
+    background: #f3f3f3;
+    border-top-left-radius: 6px;
+    border-top-right-radius: 6px;
+}
+.header-title {
+    float: left;
+    width: 100px;
+    margin: 0;
+    font-size: 18px;
+    font-weight: 500;
+    text-align: center;
+}
+.close {
+   float: right;
+   width: 16px;
+   height: 16px;
+   line-height: 25px;
+   font-size: 21px;
+   cursor: pointer;
+}
 #post-container h2 {
   text-align: center;
-  margin: 8px 0;
+  margin: 20px 0;
 }
 .post-content span {
   display: block;
-  margin: 4px 0;
+  margin: 12px 0;
   text-align: center;
 }
 .post-content {
-  width: 528px;
+  width: 515px;
   margin: 0 auto;
 }
 .post-content textarea {
   display: block;
-  width: 529px;
+  width: 515px;
   margin: 10px 0 20px;
   padding: 6px 8px;
   border-radius: 3px;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
 }
 .post-content input[type="checkbox"] {
   margin-right: 10px;
+}
+.btn {
+    display: inline-block;
+    float: right;
+    text-align: center;
+    cursor: pointer;
+    border: 1px solid #008151;
+    white-space: nowrap;
+    padding: 6px 20px;
+    font-size: 14px;
+    border-radius: 6px;
+    color: #fff;
+    background: #009a61;
+}
+.btn:hover {
+    padding: 6px 20px;
+    background: #006741;
+    border-color: #00432a;
+}
+.btn:active {
+    outline: 0;
 }
 </style>
